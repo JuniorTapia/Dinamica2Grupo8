9a54780a37f1b8459b13d07bf313d518869269dd
JuniorTapia <66886120+JuniorTapia@users.noreply.github.com>
2021-10-19 17:59:07 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const User = require('./models/User');
+const Mentor = require('./models/Mentor');
+const Mentorizados = require('./models/Mentorizados');
 const { error } = require('console');
 require('./database');
 
@@ -12,31 +13,60 @@ app.set('port', process.env.PORT|| 3000);
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-app.get('/api/user',(req,res) => {
 
-    User.find({},(err,user)=>{
+//Metodos del Mentor
+app.get('/api/mentor',(req,res) => {
+
+    Mentor.find({},(err,mentor)=>{
         if(err) return res.status(500).send({message: `Error al realizar la peticion ${err}`})
-        if(!user) return res.status(404).send({message: `No existen usuarios`})
-        res.send(200, { user });
+        if(!mentor) return res.status(404).send({message: `No existen usuarios`})
+        res.send(200, { mentor });
     });
 });
 
-app.post('/api/user', (req,res)=>{
-    console.log('POST /api/user');
+app.post('/api/mentor', (req,res)=>{
+    console.log('POST /api/mentor');
     console.log(req.body);
 
-    let user = new User();
-    user.firstname = req.body.firstname
-    user.lastname = req.body.lastname
-    user.doc_id= req.body.doc_id
+    let mentor = new Mentor();
+    mentor.firstname = req.body.firstname
+    mentor.lastname = req.body.lastname
+    mentor.doc_id= req.body.doc_id
 
-    user.save((err, userStored)=>{
+    mentor.save((err, mentorStored)=>{
         if(err) res.status(500).send({message: `Error al salvar en la base de datos: ${err}`})
 
-        res.status(200).send({user: userStored});
+        res.status(200).send({mentor: mentorStored});
     })
 });
 
+
+//Metodos del Mentorizado
+app.get('/api/mentorizados',(req,res) => {
+
+    Mentorizados.find({},(err,mentorizados)=>{
+        if(err) return res.status(500).send({message: `Error al realizar la peticion ${err}`})
+        if(!mentorizados) return res.status(404).send({message: `No existen usuarios`})
+        res.send(200, { mentorizados });
+    });
+});
+
+app.post('/api/mentorizados', (req,res)=>{
+    console.log('POST /api/mentorizados');
+    console.log(req.body);
+
+    let mentorizados = new Mentorizados();
+    mentorizados.firstname = req.body.firstname
+    mentorizados.lastname = req.body.lastname
+    mentorizados.doc_id= req.body.doc_id
+    mentorizados.doc_id_mentor=req.body.doc_id_mentor
+
+    mentorizados.save((err, mentorizadosStored)=>{
+        if(err) res.status(500).send({message: `Error al salvar en la base de datos: ${err}`})
+
+        res.status(200).send({mentorizados: mentorizadosStored});
+    })
+});
 
 app.listen(app.get('port'), ()=>{
     console.log('Server on port', app.get('port'));
--- database.js
+++ database.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/Labo2',{
+mongoose.connect('mongodb+srv://junior:junior123@dinamica2grupo8.l4goe.mongodb.net/dinamica2grupo8?retryWrites=true&w=majority',{
     //ponemos configuraciones para que el modulo si se actualiza no haya problemas cuando estes haciendo consultas a tu bd 
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(db=> console.log('Se conecto a la BD Mongo')).catch(err => console.error(err));
 
 module.exports = mongoose;
+
+//mongodb+srv://junior:junior123@dinamica2grupo8.l4goe.mongodb.net/dinamica2grupo8?retryWrites=true&w=majority
