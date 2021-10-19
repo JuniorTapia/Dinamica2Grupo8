1aff87b626529865ce87d775760202f7e49f25c3
JuniorTapia <66886120+JuniorTapia@users.noreply.github.com>
2021-10-19 17:00:10 -0500

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,11 @@
+const mongoose=require('mongoose');
+
+const {Schema} = mongoose;
+
+const UserSchema = new Schema({
+    firstname:{type: String, required:true},
+    lastname:{type: String, required:true},
+    doc_id:{type: String, required:true, unique:true}
+});
+
+module.exports = mongoose.model('User',UserSchema);
